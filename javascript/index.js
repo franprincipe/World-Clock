bc8c2e8e5aa76b2d7706823237543b1bcb760f37
Francisca Principe <franciscaprincipe0@gmail.com>
2024-05-31 13:09:52 -0400

    Added link to all cities.

--- javascript/index.js
+++ javascript/index.js
@@ -31,15 +31,17 @@ if (cityTimeZone === "current") {
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML =
-    `<div class="city">
+    citiesElement.innerHTML = `
+    <div class="city">
     <div>
     <h2>${cityName}</h2>
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
-    </div>`
-;
+    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small>
+    </div>
+    </div>
+    <a href="/">Go back...</a>
+    `;
     
 }
 
